9c477bfa9168811cd0f9d1b30bba7c55ac20f335
chinmayakb <chinmayakb2014@gmail.com>
2020-02-18 23:46:37 +0530

    HEYgit add add.c

--- add.c
+++ add.c
@@ -4,5 +4,6 @@ int main()
 {
 	printf("I'm NO ONE\n");
 	printf("You were fooled all this time thinking that it was a girl ;)\n");
+        printf("I'm Chinmaya Krishna and im done with cloning\n");
 	return 0;
 }
