d3c5e4270695253d38eeb966b660e03ab8afd723
Sujanram <sujanram0@gmail.com>
2020-02-21 21:30:05 +0530

    final commit

--- add.c
+++ add.c
@@ -6,5 +6,6 @@ int main()
 	printf("You were fooled all this time thinking that it was a girl ;)\n");
         printf("I'm Chinmaya Krishna and im done with cloning\n");
         printf("Okay bye\n");
+	printf("do you know who am I\n Well I am Shankri\n");
 	return 0;
 }
